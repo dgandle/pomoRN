6f9d0e6a88c66bb77c16b9892f299f3c3e10ef59
Doug Gandle <doug@dougs-mbp.lan>
2020-07-20 11:24:42 -0400

    app icons, app name, splash

--- android/app/src/main/java/com/pomo/MainActivity.java
+++ android/app/src/main/java/com/pomo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.pomorn;
+package com.pomo;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
@@ -17,6 +17,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "pomoRN";
+    return "Pomo";
   }
 }
